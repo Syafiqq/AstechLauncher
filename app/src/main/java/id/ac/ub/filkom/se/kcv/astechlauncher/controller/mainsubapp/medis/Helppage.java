cfdb69b3a0605066136f18de526e690edb59b498
syafiq <Syafiq,rezpector@gmail.com>
2016-10-07 13:32:27 +0700

    Update flow

--- app/src/main/java/id/ac/ub/filkom/se/kcv/astechlauncher/controller/mainsubapp/medis/Helppage.java
+++ app/src/main/java/id/ac/ub/filkom/se/kcv/astechlauncher/controller/mainsubapp/medis/Helppage.java
@@ -1,12 +1,10 @@
 package id.ac.ub.filkom.se.kcv.astechlauncher.controller.mainsubapp.medis;
 
 import android.os.Bundle;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
-import android.view.View;
 
 import id.ac.ub.filkom.se.kcv.astechlauncher.R;
 
@@ -48,6 +46,7 @@ public class Helppage extends AppCompatActivity
     private void onBackButtonPressed()
     {
         super.onBackPressed();
+        super.finish();
     }
     public boolean onOptionsItemSelected(MenuItem item)
     {
